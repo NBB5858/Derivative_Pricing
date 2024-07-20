17fdb82e19785408ac850a8acbf6e0f794b27162
Noah Bittermann <nlbittermann@gmail.com>
2024-07-20 13:53:15 -0400

    Initial commit

---
+++ Random1.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Noah bittermann on 7/20/24.
+//
+
+#include "Random1.h"
+#include <cstdlib>
+#include <cmath>
+
+#if !defined(_MSC_VER)
+    using namespace std;
+#endif
+
+double GetOneGaussianBySummation() {
+    double result=0;
+
+    for (unsigned long j=0; j<12; j++) {
+        result += rand()/static_cast<double>(RAND_MAX);
+    }
+
+    result -= 6.0;
+
+    return result;
+}
+
+double GetOneGaussianByBoxMuller() {
+
+    double x, y, sizeSquared;
+
+    // rejection sampling
+    do {
+        x = 2.0*rand()/static_cast<double>(RAND_MAX) - 1;
+        y = 2.0*rand()/static_cast<double>(RAND_MAX) - 1;
+        sizeSquared = x*x + y*y;
+    }
+    while (sizeSquared >= 1.0);
+
+    double result = x*sqrt(-2*log(sizeSquared)/sizeSquared);
+
+    return result;
+}
+
+
---
+++ Random1.h
@@ -0,0 +1,11 @@
+//
+// Created by Noah bittermann on 7/20/24.
+//
+
+#ifndef RANDOM1_H
+#define RANDOM1_H
+
+double GetOneGaussianBySummation();
+double GetOneGaussianByBoxMuller();
+
+#endif //RANDOM1_H
